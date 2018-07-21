c67debf3f8cade5146249a60aa8b04485c8f1943
tidjungs <waewprach.s@ku.th>
2018-07-21 14:49:12 +0700

    add concat string variadic function

--- stringhelper/helper.go
+++ stringhelper/helper.go
@@ -1,10 +1,20 @@
 package stringhelper
 
 import (
+	"fmt"
 	"strings"
 )
 
 // Upper is ...
 func Upper(text string) string {
 	return strings.ToUpper(text)
+}
+
+// Concat is ...
+func Concat(inputs ...string) string {
+	var result string
+	for _, input := range inputs {
+		result = fmt.Sprintf("%s%s", result, input)
+	}
+	return result
 }
